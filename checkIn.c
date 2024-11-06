e72830f2c18f2bf66726fec43cc31936cf143299
BHASVIC-darekczyzewski24 <darek.czyzewski24@students.bhasvic.ac.uk>
2024-11-06 13:56:29 +0000

    literally just created the checkin file

---
+++ checkIn.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+
+//
+// Created by darek.czyzewski24 on 11/6/2024.
+//
+
+int main(void) {
+    char fname[20];
+    char sname[20];
+    char 
+    return 0;
+}
